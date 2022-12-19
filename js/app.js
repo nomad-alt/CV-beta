671c25d35e82f755169ae8daacc53e9091911bb1
nomad-alt <nomad-alt@proton.me>
2022-12-19 17:00:25 +0100

    dark-light-mode

--- js/app.js
+++ js/app.js
@@ -1,30 +1,42 @@
-const educationList = document.querySelector('.educationList')
-const employmentList = document.querySelector('.employmentList')
+const educationList = document.querySelector(".educationList");
+const employmentList = document.querySelector(".employmentList");
 
-fetch("/json/cv.json") 
-    .then((response) => { 
-        if (response.ok) { 
-          return response.json(); 
-        } else { 
-          throw new Error("NETWORK RESPONSE ERROR"); 
-        } 
-      }) 
-       .then(data => { 
-       data.education.forEach(edu => {
-            educationList.innerHTML += `
+fetch("/json/cv.json")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => {
+    data.education.forEach((edu) => {
+      educationList.innerHTML += `
             <h3>${edu.heading}</h3>
             <li>${edu.text}</li>`;
+    });
 
-        });
-
-        data.employment.forEach(emp => {
-            employmentList.innerHTML += `
+    data.employment.forEach((emp) => {
+      employmentList.innerHTML += `
             <h3>${emp.heading}</h3>
             <li>${emp.text}</li>`;
-      });
-    })
+    });
+  });
 
-function toggleMode() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+checkDarkMode();
+
+function enableLightMode() {
+  document.body.classList.remove("dark");
+  localStorage.darkMode = false;
+}
+function enableDarkMode() {
+  document.body.classList.add("dark");
+  localStorage.darkMode = true;
+}
+function checkDarkMode() {
+  if (localStorage.darkMode) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
 }
